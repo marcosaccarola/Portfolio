8f438742f6ac039eb4d50a7da044e03785ef9b9f
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-21 00:44:40 +0100

    Navbar style and details

--- src/components/100-Bar.jsx
+++ src/components/100-Bar.jsx
@@ -1,14 +1,16 @@
-import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { Container, Image, Nav, Navbar } from 'react-bootstrap'
 import './000.css'
+import { AiOutlineMail } from 'react-icons/ai';
+import Marco from '../assets/Marco-Saccarola.jpg'
 
 const Bar=()=>{
 
     return(
-        <Navbar bg="dark" variant="dark" className='min-vw-100 bar'>
+        <Navbar bg="dark" variant='dark' className='min-vw-100 bar'>
             <Container>
-                <Navbar.Brand href="#home">my photo</Navbar.Brand>
+                <Navbar.Brand><Image className='avatar' src={Marco} /></Navbar.Brand>
                 <Nav className="ms-auto">
-                    <Nav.Link>Email icon</Nav.Link>
+                    <Nav.Link><AiOutlineMail className='icon-email' /></Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
