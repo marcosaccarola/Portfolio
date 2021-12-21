110f91b73589021bebdf12d3b7ab65fb8e87f0cd
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-21 01:17:27 +0100

    screenshot health-advantage

--- src/components/200-Central.jsx
+++ src/components/200-Central.jsx
@@ -1,11 +1,16 @@
-import { Container } from 'react-bootstrap'
+import { Col, Container, Image, Row } from 'react-bootstrap'
 import './000.css'
+import ha from '../assets/ha.png'
 
 const Central=()=>{
 
     return(
             <Container className='bg-dark con'>
-
+                <Row>
+                    <Col md={6} className=''>
+                        <Image src={ha} className='screen' />
+                    </Col>
+                </Row>
             </Container>
     )
 }
