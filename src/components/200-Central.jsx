882d71aac5ca7dc2e01a4ae7baa9c28a90afbc61
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-21 01:36:42 +0100

    call it a day

--- src/components/200-Central.jsx
+++ src/components/200-Central.jsx
@@ -10,6 +10,14 @@ const Central=()=>{
                     <Col md={6} className=''>
                         <Image src={ha} className='screen' />
                     </Col>
+                    <Col md={6} className='bg-warning'>
+                        <Col md={12} className='text-flux bg-info box-little'>
+                            something
+                        </Col>
+                        <Col md={12} className='text-flux bg-danger box-little'>
+                            something
+                        </Col>
+                    </Col>
                 </Row>
             </Container>
     )
