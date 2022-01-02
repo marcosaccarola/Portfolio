75fe8b8b3a23dd5ec89e5137b5e0862a90cc912f
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-02 23:46:55 +0100

    responsive?

--- src/components/205-Myself.jsx
+++ src/components/205-Myself.jsx
@@ -4,18 +4,12 @@ const Myself=()=>{
 
     return(
         <>
-            <Col className='mx-5 my-5'>
-                <Row><span>MERN developer</span></Row>
-                <Row className='mt-5'>name:</Row>
-                <Row className='ms-1 my-3 text-ultra-lime'>Marco Saccarola</Row>
-                <Row className='mt-5'>place:</Row>
-                <Row className='ms-1 my-3 text-ultra-lime'>northeastern Italy</Row>
-                <Row className='mt-5'>languages:</Row>
-                <Row className='ms-1 my-3 text-ultra-lime'>Javascript / Typescript</Row>
-                <Row className='mt-5'>in short:</Row>
-                <Row className='ms-1 my-3 text-ultra-lime'>Mongo / Express / React / Node</Row>
-                <Row className='mt-5'>deeply:</Row>
-                <Row className='ms-1 my-3 text-ultra-lime'>PostrgreSQl / Redux / Heroku / Vercel / GitHub</Row>
+            <Col className='mx-4 text-ultra-lime'>
+                <Row className='my-5'><span>Marco Saccarola - MERN developer</span></Row>
+                <Row className='my-2'>place: Venice, Italy</Row>
+                <Row className='my-2'>languages: Javascript, Typescript</Row>
+                <Row className='my-2'>in short: React, Redux, MongoDB, Mongoose, PostgreSQL, Sequelize, Express, Node</Row>
+                {/* <Row className='my-5'><span>Marco Saccarola - MERN developer</span></Row> */}
 
             </Col>
         </>
