5c781bb3d04e3243f14a4f17e4b32ead77dc8e26
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-03 16:28:36 +0100

    contact me component

--- src/components/206-ContactMe.jsx
+++ src/components/206-ContactMe.jsx
@@ -1,22 +1,30 @@
 import { Col, Row } from "react-bootstrap"
 
-const ContactMe=()=>{
+const ContactMe=({setIndex})=>{
 
     return(
         <Row className='text-ultra-lime'>
             <Col className='mx-4'>
-                <Row className='mx-3 py-3 my-3 justify-content-center border-blue'><span>Contact me</span></Row>
-                <Row className='mx-3 py-3 mt-5'>
-                    copy / paste my email address: marco.saccarola.dev@gmail.com
+                <Row className='mx-3 py-3 my-3 justify-content-center border-blue'>
+                    <span>Contact me</span>
                 </Row>
-                <Row className='mx-3 py-3'>or</Row>
-                <Col md={6}>
-                    <Row className='mx-3 py-3 my-5 border-blue justify-content-center'>
-                        <a href='mailto:marco.saccarola.dev@gmail.com' className='text-ultra-lime'>
-                            click here to open your email provider
-                        </a>
-                    </Row>
-                </Col>
+                <Row className='mx-3 py-3 mt-5 justify-content-center'>
+                    marco.saccarola.dev@gmail.com
+                </Row>
+                <Row className='mx-3 py-3 mt-5' >
+                    <Col md={6}>
+                        <Row className='me-5 py-3 border-blue justify-content-center'>
+                            <a href='mailto:marco.saccarola.dev@gmail.com' className='text-ultra-lime'>
+                                click here to open your email provider
+                            </a>
+                        </Row>
+                    </Col>
+                    <Col md={6}>
+                        <Row className='ms-5 py-3 border-blue justify-content-center' onClick={()=>setIndex('Menu')}>
+                            Back home
+                        </Row>
+                    </Col>
+                </Row>
             </Col>
         </Row>
     )
