7eb4efd603fd6059dcc5f909c91f1f839be0c8ce
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-23 15:34:21 +0100

    Grid of skills

--- src/components/700-Pills.jsx
+++ src/components/700-Pills.jsx
@@ -1,3 +1,4 @@
+import './600-style.css'
 import { Button, Col, Container, Row } from "react-bootstrap"
 
 
@@ -6,41 +7,43 @@ const Pills=()=>{
     return(
         <Container fluid className='mt-5'>
             <Row className='bg-black'>
-                <Col md={3} className='d-flex flex-column align-items-end'>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Javascript</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Typescript</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>HTML5</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>CSS3</Button>
+                <Col md={3} sm={6} className='d-flex flex-column align-items-end'>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Javascript</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Typescript</Button>
+                    <Button className='btn-fluid btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>HTML5</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>CSS3</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Git</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>GitHub</Button>
                 </Col>
-                <Col md={3} className='d-flex flex-column align-items-start'>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>React</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Hooks</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Redux</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Flexbox</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Bootstrap</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>REST APIs</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Postman</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Vercel</Button>
+                <Col md={3} sm={6} className='d-flex flex-column align-items-start'>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>React</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Hooks</Button>
+                    <Button className='btn-fluid btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Redux</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Flexbox</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Bootstrap</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>REST APIs</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Postman</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Vercel</Button>
                 </Col>
-                <Col md={3} className='d-flex flex-column align-items-end'>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>SQL</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>NoSQL</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Mongoose</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Sequelize</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>OAuth</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Socket.io</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Git</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>GitHub</Button>
+                <Col md={3} sm={6} className='d-flex flex-column align-items-end container-fluid'>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>SQL</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>NoSQL</Button>
+                    <Button className='btn-fluid btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Mongoose</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Sequelize</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>OAuth</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Socket.io</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Multer</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Buffer</Button>
                 </Col>
-                <Col md={3} className='d-flex flex-column align-items-start'>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Node</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Express</Button>
-                <Button className='btn-info rounded my-3 mx-3 btn-lg'>MongoDB</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>PostgreSQL</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>GoogleStrategy</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Cors</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Heroku</Button>
-                    <Button className='btn-info rounded my-3 mx-3 btn-lg'>Jest</Button>
+                <Col md={3} sm={6} className='d-flex flex-column align-items-start'>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Node</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Express</Button>
+                    <Button className='btn-fluid btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>MongoDB</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>PostgreSQL</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>GoogleStrategy</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Cors</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Jest</Button>
+                    <Button className='btn-dark rounded my-3 mx-3 btn-lg border border-light rounded-pill'>Heroku</Button>
                 </Col>
             </Row>
         </Container>
