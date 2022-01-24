d6a307a7860dd8975d1153ec62d2a402360ea331
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-24 15:01:43 +0100

    Footer

--- src/components/600-Body.jsx
+++ src/components/600-Body.jsx
@@ -3,6 +3,8 @@ import { Col, Container, Row } from "react-bootstrap"
 import avatar from '../assets/linkedin-profilo.jpg'
 import Name from "./690-Name"
 import Pills from "./700-Pills"
+import {MdAlternateEmail} from 'react-icons/md'
+import {BsLinkedin,BsGithub} from 'react-icons/bs'
 
 const Body=()=>{
 
@@ -29,6 +31,11 @@ const Body=()=>{
             </Row>
             <Container>
                 <Pills />
+            </Container>
+            <Container className='d-flex text-warning bg-black w-75 mx-auto justify-content-end px-0 my-5'>
+                <MdAlternateEmail className='text-warning me-3' size={25} onClick={()=>window.open('mailto:marco.saccarola.dev@gmail.com')} />
+                <BsLinkedin className='text-warning me-3' size={25} onClick={()=>window.open('https://www.linkedin.com/in/marcosaccarola/')} />
+                <BsGithub className='text-warning' size={25} onClick={()=>window.open('https://github.com/marcosaccarola/hex-to-base64-converter')} />
             </Container>
         </Container>
         </>
