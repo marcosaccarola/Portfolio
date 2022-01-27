d2748ceff00838a72a85ca55fc6e9233f871d4e3
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-27 19:41:49 +0100

    Remove prev version

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import Body from './components/600-Body';
 function App() {
   return (
     <div className="min-vh-100 bg-animation text-light">
-    {/* <div className="App"> */}
-      {/* <header className="App-header"> */}
-        {/* <Home /> */}
-      {/* </header> */}
-
-      {/* <Bar /> */}
       <Body />
     </div>
   );
